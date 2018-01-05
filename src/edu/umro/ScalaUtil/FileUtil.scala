9d9c917f316a2fd93500596ded3e632d0bcfdf3c
Jim Irrer <irrer@umich.edu>
2018-01-05 17:24:27 -0500

    Added zip utilities and stream copy function.

--- src/edu/umro/ScalaUtil/FileUtil.scala
+++ src/edu/umro/ScalaUtil/FileUtil.scala
@@ -1,78 +1,246 @@
 package edu.umro.ScalaUtil
 
 import java.io.File
+import java.sql.Blob
+import javax.sql.rowset.serial.SerialBlob
+import java.util.zip.ZipOutputStream
+import java.util.zip.ZipEntry
+import java.io.ByteArrayOutputStream
+import java.util.zip.ZipInputStream
+import java.io.ByteArrayInputStream
+import resource.managed
+import java.io.OutputStream
+import java.io.InputStream
+import scala.annotation.tailrec
+import edu.umro.util.Utility
+import java.io.FileOutputStream
+import java.io.FileInputStream
 
 object FileUtil {
-    /**
-     * Recursively compare two directories or files and throw an exception if their content is different.
-     *
-     * @param a
-     *            One file or directory.
-     *
-     * @param b
-     *            The other file or directory.
-     * @return
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-    def compareDirs(a: File, b: File): Unit = {
-        if (a.isDirectory && b.isDirectory) {
-            val aList = a.listFiles
-            val bList = b.listFiles
-            if (aList.length != bList.length) throw new RuntimeException("Missing file")
-            aList.map(aa => compareDirs(aa, new File(b, aa.getName())))
-        }
+
+  /**
+   * Copy the input stream to the output until the input is empty.  Note that the output is not closed.
+   *
+   * @return The number of bytes copied.
+   */
+  def copyStream(input: InputStream, output: OutputStream): Long = {
+    val len = 8 * 1024
+    val buffer = new Array[Byte](len)
+    val EOF = -1
+    @tailrec
+    def copy(size: Long): Long = {
+      val n = input.read(buffer)
+      if (n == EOF) {
+        size
+      } else {
+        output.write(buffer, 0, n)
+        copy(size + n)
+      }
+    }
+    copy(0)
+  }
+
+  /**
+   * Recursively compare two directories or files and throw an exception if their content is different.
+   *
+   * @param a
+   *            One file or directory.
+   *
+   * @param b
+   *            The other file or directory.
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  def compareDirs(a: File, b: File): Unit = {
+    if (a.isDirectory && b.isDirectory) {
+      val aList = a.listFiles
+      val bList = b.listFiles
+      if (aList.length != bList.length) throw new RuntimeException("Missing file")
+      aList.map(aa => compareDirs(aa, new File(b, aa.getName())))
+    } else {
+      if (!(a.isFile() && b.isFile() && edu.umro.util.Utility.compareFiles(a, b))) throw new RuntimeException("files not equal");
+    }
+  }
+
+  /**
+   * Recursively compare two directories or files and throw an exception if
+   * their content is different.  Any file access errors will result in a return of 'false';
+   *
+   * @param a
+   *            One file or directory.
+   *
+   * @param b
+   *            The other file or directory.
+   *
+   * @return True if the same, false if different.
+   *
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  def compareFolders(a: File, b: File): Boolean = {
+    try {
+      compareDirs(a, b);
+      true
+    } catch {
+      case t: Throwable => false
+    }
+  }
+
+  /**
+   * Replace all characters in a name that are not supported by the Windows file system.
+   *
+   * @name: File name
+   *
+   * @replacement: Character to be used instead of original.
+   *
+   * This will work for *nix systems too.  It will replace some characters that would be
+   * allowed in *nix, but using them is generally not a good idea anyway.
+   */
+  def replaceInvalidFileNameCharacters(name: String, replacement: Char) = {
+    val original = name.getBytes
+    val modified = (0 until name.size).map(i => {
+      val c = name.charAt(i)
+      c match {
+        case _ if (c == '\\') || (c == '/') || (c == ':') || (c == '*') || (c == '"') || (c == '<') || (c == '>') => replacement
+        case _ if (c > 31) && (c < 127) => c
+        case _ => replacement
+      }
+    }).toArray
+    new String(modified)
+  }
+
+  /**
+   * Given a file or directory, make a byte array out of the zipped contents.  If it is a directory, include children.
+   *
+   * @param inFileList Zip this list of files.  If a file is a directory, then zip the whole tree below it.
+   *
+   * @param excludeList: Exclude any file whose name matches this pattern.  If the file is a directory, then its children will be excluded too.
+   *
+   * @param outputStream: Write to this stream.
+   */
+  def readFileTreeToZipStream(inFileList: Seq[File], excludeList: Seq[String], outputStream: OutputStream): Unit = {
+    def includable(file: File) = {
+      val name = file.getName
+      excludeList.find(e => name.matches(e)).isEmpty
+    }
+
+    def addOneFileToZip(file: File, zipOut: ZipOutputStream, parentName: Option[String]): Unit = {
+      if (includable(file)) {
+        val entryName = if (parentName.isDefined) parentName.get + "/" + file.getName else file.getName
+        if (file.isDirectory) file.listFiles.map(f => addOneFileToZip(f, zipOut, Some(entryName)))
         else {
-            if (!(a.isFile() && b.isFile() && edu.umro.util.Utility.compareFiles(a, b))) throw new RuntimeException("files not equal");
+          val data = Utility.readBinFile(file)
+          val zipEntry = new ZipEntry(entryName)
+          zipOut.putNextEntry(zipEntry)
+          zipOut.write(data)
+          zipOut.closeEntry
+          Trace.trace("added zip entry: " + entryName)
         }
+      }
     }
 
-    /**
-     * Recursively compare two directories or files and throw an exception if
-     * their content is different.  Any file access errors will result in a return of 'false';
-     *
-     * @param a
-     *            One file or directory.
-     *
-     * @param b
-     *            The other file or directory.
-     *
-     * @return True if the same, false if different.
-     *
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-    def compareFolders(a: File, b: File): Boolean = {
-        try {
-            compareDirs(a, b);
-            true
-        }
-        catch {
-            case t: Throwable => false
+    managed(new ZipOutputStream(outputStream)) acquireAndGet {
+      zipOut =>
+        {
+          inFileList.map(inFile => addOneFileToZip(inFile, zipOut, None))
         }
     }
+  }
 
-    /**
-     * Replace all characters in a name that are not supported by the Windows file system.
-     * 
-     * @name: File name
-     * 
-     * @replacement: Character to be used instead of original.
-     * 
-     * This will work for *nix systems too.  It will replace some characters that would be
-     * allowed in *nix, but using them is generally not a good idea anyway.
-     */
-    def replaceInvalidFileNameCharacters(name: String, replacement: Char) = {
-        val original = name.getBytes
-        val modified = (0 until name.size).map(i => {
-            val c = name.charAt(i)
-            c match {
-                case _ if (c == '\\') || (c == '/') || (c == ':') || (c == '*') || (c == '"') || (c == '<') || (c == '>') => replacement
-                case _ if (c > 31) && (c < 127) => c
-                case _ => replacement
+  def readFileTreeToZipByteArray(inFileList: Seq[File], excludeList: Seq[String]): Array[Byte] = {
+    managed(new ByteArrayOutputStream) acquireAndGet {
+      baos =>
+        {
+          readFileTreeToZipStream(inFileList, excludeList, baos)
+          baos.toByteArray
+        }
+    }
+  }
+
+  /**
+   * Given a file tree, zip it up and write it to a file.
+   *
+   * @param mainFile: Top level input file
+   */
+  def readFileTreeToZipFile(inFileList: Seq[File], excludeList: Seq[String], zipFile: File): Unit = {
+    zipFile.delete
+    managed(new FileOutputStream(zipFile)) acquireAndGet {
+      fos =>
+        {
+          readFileTreeToZipStream(inFileList, excludeList, fos)
+          fos.close
+        }
+    }
+  }
+
+  /**
+   * Given an input stream, write it as a zipped tree of files.
+   *
+   * @param inputStream: Input stream.
+   *
+   * @param parentDir: Put new files under this directory.
+   *
+   */
+  def writeZipToFileTree(inputStream: InputStream, parentDir: File): Unit = {
+
+    parentDir.mkdirs
+
+    managed(new ZipInputStream(inputStream)) acquireAndGet {
+      zipIn =>
+        {
+          @tailrec
+          def next: Unit = {
+            val entry = zipIn.getNextEntry
+            if (entry != null) {
+              val file = new File(parentDir, entry.getName.replace("/", File.separator))
+              if (entry.isDirectory) file.mkdirs
+              else {
+                file.getParentFile.mkdirs
+                val fos = new FileOutputStream(file)
+                val size = copyStream(zipIn, fos)
+                Trace.trace("copyStream size: " + size)
+                fos.close
+              }
+              next
             }
-        }).toArray
-        new String(modified)
+          }
+          // Start processing
+          next
+        }
     }
+  }
+
+  /**
+   * Given a byte array containing zipped files, expand it and write to files.
+   *
+   * @param byteArray: Input bytes.
+   *
+   * @param parentDir: Put new files under this directory.
+   */
+  def writeByteArrayZipToFileTree(byteArray: Array[Byte], parentDir: File): Unit = {
+    managed(new ByteArrayInputStream(byteArray)) acquireAndGet {
+      byteArrayIn =>
+        {
+          writeZipToFileTree(byteArrayIn, parentDir)
+        }
+    }
+  }
+
+  /**
+   * Given a zip file, expand it and write to files.
+   *
+   * @param zipFile: Input zip file.
+   *
+   * @param parentDir: Put new files under this directory.
+   */
+  def writeZippedFileToFileTree(zipFile: File, parentDir: File): Unit = {
+    managed(new FileInputStream(zipFile)) acquireAndGet {
+      zipFileIn =>
+        {
+          writeZipToFileTree(zipFileIn, parentDir)
+        }
+    }
+  }
 
 }
