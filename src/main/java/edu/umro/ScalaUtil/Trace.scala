efc399fe02f982eb97cb9d0d83595f328d74ffe6
Jim Irrer <irrer@umich.edu>
2019-01-30 13:35:38 -0500

    Use better stack formatting to help Eclipse locate correct source file
    Also formatted with new indenting.

--- src/main/java/edu/umro/ScalaUtil/Trace.scala
+++ src/main/java/edu/umro/ScalaUtil/Trace.scala
@@ -8,31 +8,31 @@ import java.text.SimpleDateFormat
  */
 object Trace {
 
-    private val dateFormat = new SimpleDateFormat("HH:mm:ss.SSS")
+  private val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
 
-    private def current: String = {
-        val se = Thread.currentThread.getStackTrace()(3)
-        val line = se.getLineNumber
-        val method = se.getMethodName
-        val threadName = Thread.currentThread.getName
-        "Trace " + dateFormat.format(new Date) + " | " + threadName + " | (" + se.getFileName + ":" + se.getLineNumber + ") " + se.getMethodName
-    }
+  private def current: String = {
+    val se = Thread.currentThread.getStackTrace()(3)
+    val line = se.getLineNumber
+    val method = se.getMethodName
+    val threadName = Thread.currentThread.getName
+    "Trace " + dateFormat.format(new Date) + " | " + threadName + " | " + se.toString
+  }
 
-    /** Print current line. */
-    def trace: Unit = {
-        println(current)
-    }
+  /** Print current line. */
+  def trace: Unit = {
+    println(current)
+  }
 
-    /** Print current line and parameter value. */
-    def trace(v: Any): Unit = {
-        val text = if (v == null) "null" else v.toString
-        println(current + " : " + text)
-    }
+  /** Print current line and parameter value. */
+  def trace(v: Any): Unit = {
+    val text = if (v == null) "null" else v.toString
+    println(current + " : " + text)
+  }
 
-    /** For testing only. */
-    def main(args: Array[String]): Unit = {
-        trace("hey")
-        trace
-    }
+  /** For testing only. */
+  def main(args: Array[String]): Unit = {
+    trace("hey")
+    trace
+  }
 
 }
