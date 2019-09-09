c370332d9962c9baae917876ccce483a67f5c791
Jim Irrer <irrer@umich.edu>
2019-09-09 19:00:12 -0400

    made treeify tool that moves DICOM files into a tree

---
+++ src/main/java/edu/umro/ScalaUtil/TreeifyDicom.scala
@@ -0,0 +1,85 @@
+package edu.umro.ScalaUtil
+
+import java.io.File
+import com.pixelmed.dicom.AttributeList
+import com.pixelmed.dicom.TagFromName
+import com.pixelmed.dicom.AttributeTag
+
+object TreeifyDicom {
+
+  case class DicomObj(PatientID: String, StudyInstanceUID: String, SeriesInstanceUID: String, Modality: String, file: File) {
+
+    def makeSeriesDir(index: Int, studyDir: File): File = {
+      val seriesDir = new File(studyDir, Modality + "_" + index.formatted("%02d"))
+      if (seriesDir.isDirectory) makeSeriesDir(index + 1, studyDir)
+      else {
+        seriesDir.mkdirs
+        seriesDir
+      }
+    }
+
+    def getSeriesDir(studyIndex: Int, outDir: File) = {
+      val patDir = new File(outDir, PatientID)
+      val studyDir = new File(patDir, "Study_" + (studyIndex + 1))
+      val seriesDir = makeSeriesDir(1, studyDir)
+      seriesDir.mkdirs
+      seriesDir
+    }
+
+    def moveTo(seriesDir: File, instanceIndex: Int, outDir: File) = {
+      val destFile = new File(seriesDir, Modality + "_" + (instanceIndex + 1).formatted("%03d") + ".dcm")
+      file.renameTo(destFile)
+      print(".")
+    }
+  }
+
+  private def readFile(dicomFile: File) = {
+    try {
+      val al = new AttributeList
+      al.read(dicomFile)
+      def getAttr(tag: AttributeTag) = al.get(tag).getSingleStringValueOrEmptyString
+
+      Some(new DicomObj(getAttr(TagFromName.PatientID), getAttr(TagFromName.StudyInstanceUID), getAttr(TagFromName.SeriesInstanceUID), getAttr(TagFromName.Modality), dicomFile))
+    } catch {
+      case t: Throwable => {
+        println("Unable to read file as DICOM.  Ignoring: " + dicomFile.getAbsolutePath)
+        None
+      }
+    }
+  }
+
+  private def moveSeries(seriesList: Seq[DicomObj], studyIndex: Int, seriesIndex: Int, outDir: File) = {
+    val seriesDir = seriesList.head.getSeriesDir(studyIndex, outDir)
+    seriesList.zipWithIndex.map(df => df._1.moveTo(seriesDir, df._2, outDir))
+  }
+
+  private def moveStudy(study: Seq[DicomObj], studyIndex: Int, outDir: File) = {
+    val seriesList = study.groupBy(_.SeriesInstanceUID).map(kv => kv._2).toSeq
+    seriesList.zipWithIndex.map(series => moveSeries(series._1, studyIndex, series._2, outDir))
+  }
+
+  private def movePatient(patient: Seq[DicomObj], outDir: File) = {
+    val studyList = patient.groupBy(_.StudyInstanceUID).map(kv => kv._2).toSeq
+    studyList.zipWithIndex.map(study => moveStudy(study._1, study._2, outDir))
+  }
+
+  def main(args: Array[String]): Unit = {
+    try {
+      val start = System.currentTimeMillis
+      val inDir = new File(args(0))
+      val outDir = new File(args(1))
+      val fileList = inDir.listFiles.toSeq
+      println("Number of files to process: " + fileList.size)
+      val dicomFileList = fileList.map(f => readFile(f)).flatten
+      println("Number of DICOM files to process: " + dicomFileList.size)
+      val patientList = dicomFileList.groupBy(_.PatientID).map(kv => kv._2).toSeq
+
+      patientList.map(patient => movePatient(patient, outDir))
+      println("\nDone.  Elapsed ms: " + (System.currentTimeMillis - start))
+    } catch {
+      case t: Throwable =>
+        t.printStackTrace
+    }
+  }
+
+}
