432fbbfadef5bcf596c8d8b422103b2fa76e5d17
Jim Irrer <irrer@umich.edu>
2019-11-26 18:26:50 -0500

    add constructor from XML Node

--- src/main/java/edu/umro/ScalaUtil/PACS.scala
+++ src/main/java/edu/umro/ScalaUtil/PACS.scala
@@ -1,11 +1,27 @@
 package edu.umro.ScalaUtil
 
+import scala.xml.Node
+
 /**
  * Define a DICOM device.
  */
 class PACS(val aeTitle: String, val host: String, val port: Int) {
 
-  def this(composite: String) = this(composite.split(":")(0), composite.split(":")(1), composite.split(":")(2).toInt);
+  /**
+   * Construct from a string of the format aetitle:host:port
+   */
+  def this(composite: String) = this(composite.split(":")(0), composite.split(":")(1), composite.split(":")(2).toInt)
+
+  /**
+   * Construct from XML of the form
+   *
+   *     <Ignored AETitle='myAETitle' Host='141.214.125.209' Port='15678' />
+   */
+  def this(node: Node) = this(PACS.getAttr(node, "AETitle"), PACS.getAttr(node, "Host"), PACS.getAttr(node, "Port").toInt)
 
   override def toString = aeTitle + " = " + host + ":" + port
+}
+
+object PACS {
+  def getAttr(node: Node, name: String) = (node \ ("@" + name)).text.toString
 }
