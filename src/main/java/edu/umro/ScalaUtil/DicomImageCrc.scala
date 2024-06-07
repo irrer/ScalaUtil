cb97a9cffa17244c2bb4622c08f9dbf9c600af29
Jim Irrer <irrer@umich.edu>
2024-06-07 15:49:32 -0400

    Made utility for calculating the 64-bit CRC of the image bytes of a DICOM file.
    This can be used to determine whether two (or more) DICOM files have the same image.  This is useful for when there are changes in header data but the image stays the same.
    
    This is a CRC, so it is not cryptographically secure.

---
+++ src/main/java/edu/umro/ScalaUtil/DicomImageCrc.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2021 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.umro.ScalaUtil
+
+import com.pixelmed.dicom.AttributeList
+import com.pixelmed.dicom.OtherByteAttribute
+import com.pixelmed.dicom.OtherLongAttribute
+import com.pixelmed.dicom.OtherWordAttribute
+import edu.umro.DicomDict.TagByName
+
+import java.io.File
+
+/**
+  * Create a hash of the image pixels for all given DICOM files.
+  */
+object DicomImageCrc {
+
+  private var count = 0
+
+  /**
+    * If the given files is DICOM image file, then print the file name and a CRC of its image bytes.
+    *
+    * @param file DICOM file being input.
+    * @param al   Attribute list of DICOM file.
+    */
+  private def alToCrc(file: File, al: AttributeList): Unit = {
+    val pixelData = al.get(TagByName.PixelData)
+    if (pixelData != null) {
+      //val byteArray = pixelData.getShortValues()
+
+      var crc: Long = 0
+      if (pixelData.isInstanceOf[OtherWordAttribute]) {
+        val shortArray = pixelData.getShortValues
+        shortArray.foreach(b => { crc = (crc << 1) + (b.toInt & 0xffff) })
+      }
+
+      if (pixelData.isInstanceOf[OtherByteAttribute]) {
+        val shortArray = pixelData.getShortValues
+        shortArray.foreach(b => { crc = (crc << 1) + (b.toInt & 0xffff) })
+      }
+
+      if (pixelData.isInstanceOf[OtherLongAttribute]) {
+        val intArray = pixelData.getIntegerValues
+        intArray.foreach(b => { crc = (crc << 1) + (b & 0xffffffff) })
+      }
+
+      val crcText = "%016x".format(crc)
+      val filePath = file.getAbsolutePath
+      println(s"$filePath : $crcText")
+
+      count = count + 1
+    }
+  }
+
+  /**
+    * If a file is DICOM, then make text file for it.
+    *
+    * @param dicomFile DICOM file being input.
+    */
+  private def readFile(dicomFile: File): Unit = {
+    try {
+      val al = new AttributeList
+      al.read(dicomFile)
+      alToCrc(dicomFile, al)
+    } catch {
+      case _: Throwable =>
+        System.err.println("Unable to read file as DICOM.  Ignoring: " + dicomFile.getAbsolutePath)
+    }
+  }
+
+  /**
+    * Recursively search a file tree for DICOM files.  For each, write a text file in the same directory.
+    *
+    * @param file Top level file.  May be a file or directory.
+    */
+  private def processFilesInTree(file: File): Unit = {
+    if (file.isDirectory)
+      file.listFiles.foreach(f => processFilesInTree(f))
+    else
+      readFile(file)
+  }
+
+  def main(args: Array[String]): Unit = {
+    try {
+      val start = System.currentTimeMillis
+      if (args.isEmpty)
+        println("Create text versions of DICOM files.\nUsage: d2text myFolder myFile.dcm ... ")
+      args.foreach(a => processFilesInTree(new File(a)))
+      processFilesInTree(new File("""D:\tmp\aqa\GapSkew\d2"""))
+      println("\nDone.  Elapsed ms: " + (System.currentTimeMillis - start) + "    number of files written: " + count)
+    } catch {
+      case t: Throwable =>
+        t.printStackTrace()
+    }
+  }
+
+}
